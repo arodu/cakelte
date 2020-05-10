a318475e4504c091f337c1fa1fec3b4a39d56b36
Alberto Rodriguez <arodu.dev@gmail.com>
2020-05-10 18:59:16 -0400

    update

--- src/View/CakeLteTrait.php
+++ src/View/CakeLteTrait.php
@@ -9,8 +9,6 @@ use Cake\Core\Exception\Exception;
 trait CakeLteTrait{
 
   protected $settings = [
-      'layout' => 'CakeLte.starter',
-
     // configure layout options
       'appName' => 'Cake<b>LTE</b>', // [string] default='Cake<b>LTE</b>'
       'appLogo' => 'CakeLte.cake.icon.png', // [string] default='CakeLte.cake.icon.png'
@@ -22,8 +20,6 @@ trait CakeLteTrait{
 
   public function initializeCakeLte(array $options = []): void {
     $this->settings = array_merge($this->settings, $options);
-
-    $this->layout = $this->settings['layout'];
 
     $this->loadHelper('Form', [
       'templates' => $this->settings['form-templates'],
--- src/View/CakeLteView.php
+++ src/View/CakeLteView.php
@@ -8,6 +8,8 @@ use Cake\View\View;
 class CakeLteView extends View{
   use CakeLteTrait;
 
+  public $layout = 'CakeLte.starter';
+
   public function initialize(): void{
       parent::initialize();
       $this->initializeCakeLte();
