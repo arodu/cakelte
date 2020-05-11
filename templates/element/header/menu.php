a159cb8f52007d414fb75968bae339d1b85998d2
Alberto Rodriguez <arodu.dev@gmail.com>
2020-05-11 16:20:05 -0400

    update

--- templates/element/header/menu.php
+++ templates/element/header/menu.php
@@ -2,7 +2,7 @@
   <?= $this->Html->link(__('Home'), '/', ['class'=>'nav-link']) ?>
 </li>
 <li class="nav-item d-none d-sm-inline-block">
-  <?= $this->Html->link(__('Debug'), ['controller'=>'Pages', 'action'=>'debug', 'plugin'=>'CakeLte'], ['class'=>'nav-link']) ?>
+  <?= $this->Html->link(__('Debug'), '/cake-lte/debug', ['class'=>'nav-link']) ?>
 </li>
 <li class="nav-item d-none d-sm-inline-block">
   <?= $this->Html->link(__('Theme'), '/cake_lte/AdminLTE/index.html', ['class'=>'nav-link']) ?>
