156eff1302c7959efa867808d93dd1ebcb223c3f
Alberto Rodriguez <arodu.dev@gmail.com>
2022-10-28 17:26:39 -0400

    fix menu theme

--- templates/element/header/menu.php
+++ templates/element/header/menu.php
@@ -5,5 +5,5 @@
   <?= $this->Html->link(__('Debug'), '/cake_lte/debug', ['class' => 'nav-link']) ?>
 </li>
 <li class="nav-item d-none d-sm-inline-block">
-  <?= $this->Html->link(__('Theme'), '/cake_lte/AdminLTE/index.html', ['class' => 'nav-link']) ?>
+  <?= $this->Html->link(__('Theme'), '/adminlte/index.html', ['class' => 'nav-link']) ?>
 </li>
