9bd9935eefa88b415f9f4d3966fbd612f19ed744
Alberto Rodriguez <arodu.dev@gmail.com>
2025-08-23 11:43:56 -0400

    feat: add login layout and form with reCAPTCHA and remember me functionality

---
+++ templates/element/plugin/CakeDC/Users/Users/login.php
@@ -0,0 +1,96 @@
+<?php
+
+use Cake\Core\Configure;
+
+$this->setLayout('CakeLte.login');
+
+?>
+
+<p class="login-box-msg"><?= __('Sign in to start your session') ?></p>
+<?= $this->Form->create() ?>
+
+<?= $this->Form->control('username', ['label' => __d('cake_d_c/users', 'Username'), 'required' => true]) ?>
+<?= $this->Form->control('password', ['label' => __d('cake_d_c/users', 'Password'), 'required' => true]) ?>
+
+<div class="row">
+    <div class="col">
+        <?php
+        if (Configure::read('Users.reCaptcha.login')) {
+            echo $this->User->addReCaptcha();
+        }
+        ?>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col-8">
+        <div class="form-check">
+            <?php
+            if (Configure::read('Users.RememberMe.active')) {
+                echo $this->Form->control(Configure::read('Users.Key.Data.rememberMe'), [
+                    'type' => 'checkbox',
+                    'label' => __d('cake_d_c/users', 'Remember me'),
+                    'checked' => Configure::read('Users.RememberMe.checked')
+                ]);
+            }
+            ?>
+        </div>
+    </div>
+    <!-- /.col -->
+    <div class="col-4">
+        <div class="d-grid gap-2">
+            <?= $this->User->button(__('Sign In'), ['class' => 'btn btn-primary']); ?>
+        </div>
+    </div>
+    <!-- /.col -->
+</div>
+<!--end::Row-->
+<?= $this->Form->end() ?>
+<div class="social-auth-links text-center mb-3 d-grid gap-2">
+    <?= implode(' ', $this->User->socialLoginList()); ?>
+</div>
+<!-- /.social-auth-links -->
+<?php
+$registrationActive = Configure::read('Users.Registration.active');
+?>
+<?php if ($registrationActive) : ?>
+    <p class="mb-1">
+        <?= $this->Html->link(__('Register a new membership'), ['action' => 'register'], ['class' => 'text-center']) ?>
+    </p>
+<?php endif; ?>
+
+<?php if (Configure::read('Users.Email.required')) : ?>
+    <p class="mb-1">
+        <?= $this->Html->link(__('I forgot my password'), ['action' => 'requestResetPassword'], ['class' => 'text-center']) ?>
+    </p>
+    <?php if (Configure::read('OneTimeLogin.enabled')) : ?>
+        <p class="mb-1">
+            <?= $this->Html->link(
+                __('Send me a login link'),
+                ['plugin' => 'CakeDC/Users', 'controller' => 'Users', 'action' => 'requestLoginLink'],
+                ['allowed' => true, 'escape' => false]
+            ); ?>
+        </p>
+    <?php endif; ?>
+<?php endif; ?>
+
+
+<style>
+    .btn-google {
+        background-color: #db4437;
+        color: white;
+    }
+    .btn-google:hover {
+        background-color: #c33d2e;
+        color: white;
+    }
+
+    .btn-github {
+        background-color: #333;
+        color: white;
+    }
+    .btn-github:hover {
+        background-color: #292b2c;
+        color: white;
+    }
+</style>
---
+++ templates/layout/login.php
@@ -0,0 +1,73 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <?= $this->Html->charset() ?>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>
+        <?= $this->CakeLte->get('appName') ?> |
+        <?= $this->fetch('title') ?>
+    </title>
+    <?= $this->Html->meta('icon') ?>
+    <?= $this->CakeLte->renderCss() ?>
+    <?= $this->fetch('meta') ?>
+    <?= $this->fetch('css') ?>
+</head>
+<!--begin::Body-->
+
+<body class="login-page bg-body-secondary">
+    <div class="login-box">
+        <div class="login-logo">
+            <?= $this->Html->link($this->CakeLte->get('appName'), $this->Url->build('/')) ?>
+        </div>
+        <!-- /.login-logo -->
+        <div class="card">
+            <div class="card-body login-card-body">
+                <?= $this->fetch('content') ?>
+            </div>
+            <!-- /.login-card-body -->
+        </div>
+    </div>
+    <!-- /.login-box -->
+    <!--begin::Third Party Plugin(OverlayScrollbars)-->
+    <script
+        src="https://cdn.jsdelivr.net/npm/overlayscrollbars@2.11.0/browser/overlayscrollbars.browser.es6.min.js"
+        crossorigin="anonymous"></script>
+    <!--end::Third Party Plugin(OverlayScrollbars)--><!--begin::Required Plugin(popperjs for Bootstrap 5)-->
+    <script
+        src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+        crossorigin="anonymous"></script>
+    <!--end::Required Plugin(popperjs for Bootstrap 5)--><!--begin::Required Plugin(Bootstrap 5)-->
+    <script
+        src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.min.js"
+        crossorigin="anonymous"></script>
+    <!--end::Required Plugin(Bootstrap 5)--><!--begin::Required Plugin(AdminLTE)-->
+    <script src="../js/adminlte.js"></script>
+    <!--end::Required Plugin(AdminLTE)--><!--begin::OverlayScrollbars Configure-->
+    <script>
+        const SELECTOR_SIDEBAR_WRAPPER = '.sidebar-wrapper';
+        const Default = {
+            scrollbarTheme: 'os-theme-light',
+            scrollbarAutoHide: 'leave',
+            scrollbarClickScroll: true,
+        };
+        document.addEventListener('DOMContentLoaded', function() {
+            const sidebarWrapper = document.querySelector(SELECTOR_SIDEBAR_WRAPPER);
+            if (sidebarWrapper && OverlayScrollbarsGlobal?.OverlayScrollbars !== undefined) {
+                OverlayScrollbarsGlobal.OverlayScrollbars(sidebarWrapper, {
+                    scrollbars: {
+                        theme: Default.scrollbarTheme,
+                        autoHide: Default.scrollbarAutoHide,
+                        clickScroll: Default.scrollbarClickScroll,
+                    },
+                });
+            }
+        });
+    </script>
+    <!--end::OverlayScrollbars Configure-->
+    <!--end::Script-->
+</body>
+<!--end::Body-->
+
+</html>
