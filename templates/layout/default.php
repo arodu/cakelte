2acf030d6947ae71674bbbd979b4bab07f06b67f
Alberto Rodriguez <arodu.dev@gmail.com>
2025-12-07 11:02:34 -0400

    fix: sanitize title output in layout template to prevent XSS

--- templates/layout/default.php
+++ templates/layout/default.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>
         <?= $this->CakeLte->get('appName') ?> |
-        <?= $this->fetch('title') ?>
+        <?= strip_tags($this->fetch('title')) ?>
     </title>
     <?= $this->Html->meta('icon') ?>
     <?= $this->CakeLte->renderCss() ?>
