9d200536a2ca7605bbb60cc71e1203399884eb17
Alberto Rodriguez <arodu.dev@gmail.com>
2026-02-10 12:09:10 -0400

    fix: add conditional rendering for headerEnd in header template

--- templates/element/content/header.php
+++ templates/element/content/header.php
@@ -9,6 +9,10 @@
         <h3 class="mb-0"><?= $this->fetch('title') ?></h3>
     </div>
     <div class="col-sm-6">
-        <?= $this->Breadcrumbs->render(['class' => 'breadcrumb float-sm-end']) ?>
+        <?php if ($this->fetch('headerEnd')): ?>
+            <?= $this->fetch('headerEnd') ?>
+        <?php else: ?>
+            <?= $this->Breadcrumbs->render(['class' => 'breadcrumb float-sm-end']) ?>
+        <?php endif; ?>
     </div>
 </div>
